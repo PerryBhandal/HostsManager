71dd7b6fed3a44321da52de2c4ce2e94158fb62e
Perry Bhandal <perry.bhandal@gmail.com>
2014-11-29 04:27:59 -0500

    Added parameter processing.

--- hostsManager.rb
+++ hostsManager.rb
@@ -1,1 +1,22 @@
+require_relative "config.rb"
 
+def enableBlocking 
+
+end
+
+def disableBlocking
+
+end
+
+if ARGV.length != 1
+  puts "Error: No enable/disable parameter provided."
+else
+  stateParam = ARGV[0].downcase
+  if stateParam == "enable"
+    enableBlocking()
+  elsif stateParam == "disable"
+    disableBlocking()
+  else
+    puts "Error: %s is not a valid enable/disable parameter." % (stateParam)
+  end
+end
